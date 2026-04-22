83ac050f72d46cd0d662a30d4495d59adde744a6
artem_kruzhkov <artkruzhkov@gmail.com>
2026-04-22 14:27:40 +0300

    added gallery section on main page

--- js/main.js
+++ js/main.js
@@ -177,6 +177,22 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+$(document).ready(function () {
+  $('.client-gallery').owlCarousel({
+    loop: true,
+    margin: 24,
+    nav: true,
+    dots: false,
+    autoplay: false,
+    responsive: {
+      0: { items: 1 },
+      640: { items: 2 },
+      1024: { items: 3 },
+      1280: { items: 4 },
+    },
+  });
+});
+
 // ======================
 // Search cars redirect
 // ======================
